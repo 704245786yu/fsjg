7941aa223510b3eb806e174aae1b3434e64ebdd0
zy company <704245786@qq.com>
2016-07-05 16:54:08 +0800

    add basicUser

--- src/com/basic/po/CustomerAbstract.java
+++ src/com/basic/po/CustomerAbstract.java
@@ -3,23 +3,11 @@ package com.basic.po;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public abstract class CustomerAbstract {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	protected Integer id;
-
-	@Column(name="user_name")
-	private String userName;	//用户名，登录平台用。企业用户可为空，个人用户不可为空
-	
-	private String password;
-	
 	private Long province;	//省
 	
 	private Long city;	//市
@@ -30,8 +18,6 @@ public abstract class CustomerAbstract {
 	
 	@Column(name="detail_addr")
 	private String detailAddr;	//详细地址
-	
-	private String telephone;	//手机号,手机号需唯一,用户可通过手机号登录系统
 	
 	private Long qq;
 	
@@ -52,36 +38,6 @@ public abstract class CustomerAbstract {
 	@Column(name="audit_time")
 	private Date auditTime;	//审核时间
 	
-	@Column(name="create_by")
-	private Integer createBy;	//创建人。0：表示用户自行注册，非0表示由平台管理人员录入
-	
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	@Column(name = "create_time")
-	private Date createTime;	//创建时间(注册时间)
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	public Long getProvince() {
 		return province;
@@ -121,14 +77,6 @@ public abstract class CustomerAbstract {
 
 	public void setDetailAddr(String detailAddr) {
 		this.detailAddr = detailAddr;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
 	}
 
 	public Long getQq() {
@@ -187,20 +135,4 @@ public abstract class CustomerAbstract {
 		this.auditTime = auditTime;
 	}
 
-	public Integer getCreateBy() {
-		return createBy;
-	}
-
-	public void setCreateBy(Integer createBy) {
-		this.createBy = createBy;
-	}
-
-	public Date getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-	
 }
