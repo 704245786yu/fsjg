73ca98d72d75ba0c4763d1c893d82643e279b1f1
zy company <704245786@qq.com>
2016-08-16 17:21:42 +0800

    delete auth

--- src/com/auth/model/Principal.java
+++
@@ -1,8 +0,0 @@
-package com.auth.model;
-
-/**主体
- * 声明式接口，没有任何方法，实现了这个接口的所有的类都是权限控制的主体
- * */
-public interface Principal {
-
-}
--- src/com/auth/model/SystemResource.java
+++
@@ -1,8 +0,0 @@
-package com.auth.model;
-
-/**系统资源
- * 资源对象实现的声明式接口
- * */
-public interface SystemResource {
-
-}
